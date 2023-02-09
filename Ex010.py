8c34cae51e80c21774b4e57a3216a040d07cb2c3
guga.hub <85652431+guga-hub@users.noreply.github.com>
2023-02-09 14:42:36 -0300

    Delete Ex010.py

--- Ex010.py
+++
@@ -1,7 +0,0 @@
-# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira
-# e mostre quantos Dólares ela pode comprar. Considere U$1,00 = R$3,27.
-
-reais = float(input('Digite quanto tem atualmente em reais: '))
-quantidade_de_dolares = reais / 3.27
-
-print('Com esse dinheiro você consegue comprar U${:.3f}'.format(quantidade_de_dolares))
